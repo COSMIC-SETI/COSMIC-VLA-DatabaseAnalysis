7ae602c20a9fb7c02a0118ce0e8792030335af9d
talonmyburgh <myburgh.talon@gmail.com>
2025-02-19 14:16:33 +0000

    ++ plot histogram

--- src/cosmic_database_analysis/average_grade_per_observation.py
+++ src/cosmic_database_analysis/average_grade_per_observation.py
@@ -2,6 +2,10 @@ from cosmic_database import entities
 from cosmic_database.engine import CosmicDB_Engine
 import sqlalchemy
 from sqlalchemy import func  # Import func from sqlalchemy
+import numpy as np
+import matplotlib.pyplot as plt
+
+VLASS_VERSION = '3.1'
 
 """
 BASIC REQUIRED STUFF TO GET DATABASE ACCESS
@@ -30,7 +34,7 @@ first_scan_subquery = (
         observation_id_expr.label('observation_id'),
         func.min(scan_number_expr).label('first_scan_number')
     )
-    .where(entities.CosmicDB_Observation.scan_id.like('%VLASS3.1%'))
+    .where(entities.CosmicDB_Observation.scan_id.like(f'%VLASS{VLASS_VERSION}%'))
     .group_by(observation_id_expr)
     .subquery()
 )
@@ -50,12 +54,9 @@ query = (
         (observation_id_expr == first_scan_subquery.c.observation_id) &
         (scan_number_expr != first_scan_subquery.c.first_scan_number)
     )
-    .where(entities.CosmicDB_Observation.scan_id.like('%VLASS3.1%'))
+    .where(entities.CosmicDB_Observation.scan_id.like(f'%VLASS{VLASS_VERSION}%'))
     .group_by(observation_id_expr)
 )
-
-# Print the query for debugging
-print(query)
 
 with cosmicdb_engine.session() as session:
     results = session.execute(query).fetchall()
@@ -64,3 +65,22 @@ with cosmicdb_engine.session() as session:
 for result in results:
     observation_id, average_overall_grade = result
     print(f"Observation ID: {observation_id}, Average Overall Grade: {average_overall_grade}")
+
+len_results=len(results)
+observation_ids = ['']*len_results
+average_grades = [0]*len_results
+
+for i, result in enumerate(results):
+    observation_ids[i]=result[0]
+    average_grades[i]=result[1]
+
+# Define the bin edges
+bins = [i * 0.1 for i in range(11)]
+
+# Plot the histogram
+plt.hist(average_grades, bins=bins, edgecolor='black')
+plt.xlabel('Average Overall Grade')
+plt.ylabel('Number of Observations')
+plt.title(f'Histogram of VLASS{VLASS_VERSION} Observations by Average Overall Grade')
+
+plt.savefig(f'average_grade_per_observation_vlass{VLASS_VERSION}.png')
